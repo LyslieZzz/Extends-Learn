85dd02f8e49092f462f6d4c09563ca07ef649e11
LyslieZzz <onlylyzzz@163.com>
2025-01-15 01:14:28 +0800

    test push3

--- com.zwy/src/generics_class/Test.java
+++ com.zwy/src/generics_class/Test.java
@@ -4,10 +4,10 @@ public class Test {
     public static void main(String[] args) {
 MyArrayList<String> ele = new MyArrayList<>();
         ele.add("zwy");
-        ele.add("Computer Science");
         ele.add("Electronic Commerce");
+        ele.add("Computer Science");
         ele.add("Data Science");
-        String a = ele.get(1);//根据索引提取ele中的值
+        String a = ele.get(2);//根据索引提取ele中的值
         System.out.println(a);
     }
 }
